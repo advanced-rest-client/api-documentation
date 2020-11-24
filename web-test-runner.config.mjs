0b14a36db83c8c11e82fe042257c3c9b1b60d614
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2020-11-24 14:50:07 -0300

    chore: change web test runner config to load codemirror

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,26 +1,19 @@
 export default {
-	files: 'test/**/*.test.js',
-	nodeResolve: true,
-	middleware: [
-		function rewriteBase(context, next) {
-			if (context.url.indexOf('/base') === 0) {
-				context.url = context.url.replace('/base', '')
-			}
-			return next();
-		}
-	],
-	plugins: [
-		{
-		  name: 'provide-codemirror',
-		  transform(context) {
-			if (context.path === '/') {
-			  const transformedBody = context.body.replace(
-				'</head>',
-				'<script src="./node_modules/codemirror/lib/codemirror.js"></script></head>',
-			  );
-			  return transformedBody;
-			}
-		  },
-		},
-	  ],
+  files: 'test/**/*.test.js',
+  nodeResolve: true,
+  middleware: [
+    function rewriteBase(context, next) {
+      if (context.url.indexOf('/base') === 0) {
+        context.url = context.url.replace('/base', '');
+      }
+      return next();
+    }
+  ],
+  testRunnerHtml: (testFramework) =>
+    `<html>
+		<body>
+		  <script src="node_modules/codemirror/lib/codemirror.js"></script>
+		  <script type="module" src="${testFramework}"></script>
+		</body>
+	  </html>`
 };
